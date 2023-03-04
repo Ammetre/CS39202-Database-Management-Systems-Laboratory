66e06658d2478e7da3ea307b945a624d38e46edb
ammetre <aritramitra2002@kgpian.iitkgp.ac.in>
2023-03-04 11:18:51 +0530

    Added Backend functions:I need a lot of help;"

--- A4/miniproject/hospital/views.py
+++ A4/miniproject/hospital/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth import views as auth_views
 # Create your views here.
+def home(request):
+    return HttpResponse('<h2>Hi</h2><br/><h1>No</h1>')
+class MyLoginView(auth_views.LoginView):
+    template_name = 'login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
--- A4/miniproject/miniproject/urls.py
+++ A4/miniproject/miniproject/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
+import hospital.views as Hosp_views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',Hosp_views.MyLoginView.as_view(),name='myUser-login'),    #login page   
+    # path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='myUser-logout'), #logout page
+    # path('password-reset/',auth_views.PasswordResetView.as_view(template_name='password_reset.html'),name='password_reset'), #password reset (defunct)
+    # path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'), #password reset (defunct)
+    # path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'), #password reset (defunct)
+    # path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'), #password reset (defunct)
+    # path('api/',include('api.urls')),   #api
+    # path('list_codes/',User_views.listcodes,name='myUser-listcodes'),   #list of codes
+    # path('download/',User_views.download_data,name='myUser-download'),  #download all codes in csv
+    # path('password/',User_views.change_password,name='change_password'),    #change password
+    # path('detail_code/',User_views.detailcodes,name='myUser-detailcode'),   #detail of a specific code (default 0th)
+    # path('detail_code/<int:index>',User_views.detailcodes,name='User-detailcode'),  #detail of ith code
+    # path('',User_views.course,name='Course-home'),  #homepage(default 0th)
 ]

48b5112717c7a36e5df2a75419c371ec88890dfd
anubhavdhar <anubhavdhar@kgpian.iitkgp.ac.in>
2023-03-04 11:48:36 +0530

    added more things in doctor.html

--- A4/frontend/doctor.js
+++ A4/frontend/doctor.js
@@ -1,5 +1,7 @@
 // Subham Ghosh, Aritra Mitra, Anubhav Dhar
 
+let PID = -1;
+
 function getName(EID){
 	// do database query here
 	return "Name(" + EID + ")";
@@ -16,9 +18,42 @@ window.onload = function(){
 	document.getElementById('doctor-name').innerHTML += " " + getName(data.eid);
 }
 
-function getPatientInfo(PID){
+function getPatientInfo(){
 	// do database query here
-	return "<b>Patient Information</b><br> <ul style = \"color: #D61355; font-size: 23px; font-family: \'Old Standard TT\';\"> <li> Patient ID : " + PID + "</ul>";
+	return `<b>Patient Information</b><br> 
+				<ul style = \"color: #D61355; font-size: 23px; font-family: \'Old Standard TT\';\"> 
+					<li> Patient ID : ` + PID + 
+				`</ul>`;				
+}
+
+function geneterateNewRID(){
+	// do database query to get distinct remedy ID
+	return 0;
+}
+
+function treatmentForm(){
+		return `<div>
+					<form style = \"font-size: 23px; font-family: \'Old Standard TT\'; padding-left: 20px;\" id = \"treatment-form\">
+						<br>
+						<p style = \"margin-right:5px; margin-top: 8px; color: #D61355;\">Remedy ID: <span style = \"color:black\">` + geneterateNewRID() + `</span></label>
+						<br>
+						<label for = \"treatment\" style = \"display: block; margin-right:5px; margin-top: 8px; color: #D61355;\">Treatment:</label>
+						<textarea rows = "4" cols = "50" type = \"text\" name = \"treatment\" id = \"treatment\" style = \"font-size: 23px; font-family: \'Noto Serif\'; border-radius: 10px; margin-top: 5px\"></textarea>
+						<br>
+						<label for = \"treatment-date\" style = \"margin-right:5px; margin-top: 8px; color: #D61355;\">Date:</label>
+						<input type = \"date\" name = \"treatment-date\" id = \"treatment-date\" style = \"font-size: 23px; font-family: \'Noto Serif\'; border-radius: 10px; margin-top: 5px\">
+						<br><br>
+					</form>
+				</div>
+				<br>
+				<div style = \"border: 2px solid white; margin: 10px; background: #F94A29; font-family: \'Old Standard TT\'; color: white; font-size:23px; width:250px; padding: 10px; border-radius: 10px;\" align = center onclick=\"issueTreatment()\">Schedule Treamtment</div>
+				`;
+}
+
+function issueTreatment(){
+	const treatmentPrescribed = document.forms['treatment-form'].treatment.value;
+	const treatmentDate = document.forms['treatment-form']['treatment-date'].value;
+	alert(getName(PID) + ' is prescribed ' + treatmentPrescribed + " on " + treatmentDate);
 }
 
 function notFound(PID){
@@ -27,11 +62,11 @@ function notFound(PID){
 }
 
 function FetchPatient(){
-	const PID = document.forms['patient-form'].PID.value;
+	PID = document.forms['patient-form'].PID.value;
 	const patientInfo = document.getElementById('patient-info');
 	if(notFound(PID) == true){
 		patientInfo.innerHTML = "Patient Not Found!";
 		return;
 	}
-	patientInfo.innerHTML = getPatientInfo(PID);
+	patientInfo.innerHTML = getPatientInfo(PID) + treatmentForm();
 }

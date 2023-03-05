73b5085c9480c4e993545093876ef4c6aa33d3fc
anubhavdhar <anubhavdhar@kgpian.iitkgp.ac.in>
2023-03-05 20:41:47 +0530

    patient info in doctor works completely!

--- A4/frontend/doctor.js
+++ A4/frontend/doctor.js
@@ -1,10 +1,10 @@
 // Subham Ghosh, Aritra Mitra, Anubhav Dhar
 
-let PID = -1;
+let patientInfoReceived;
 
 function getName(EID){
 	// do database query here
-	return "Name(" + EID + ")";
+	return patientInfoReceived.Name;
 }
 
 window.onload = function(){
@@ -18,15 +18,7 @@ window.onload = function(){
 	document.getElementById('doctor-name').innerHTML += " Doctor " + getName(data.eid);
 }
 
-function getPatientInfo(){
-	// do database query here
-	return `<b>Patient Information</b><br>
-				<ul style = \"color: #db3e04; font-size: 25px; font-family: \'Inter\';\">
-					<li> Patient ID : ` + PID +
-				`</ul>`;
-}
-
-function geneterateNewRID(){
+function generateNewRID(){
 	// do database query to get distinct remedy ID
 	return 0;
 }
@@ -36,7 +28,7 @@ function treatmentForm(){
 					<br>
 					<div>
 					<form style = \"font-size: 25px; font-family: \'Inter\'; padding-left: 20px;\" id = \"treatment-form\">
-						<p class = \"badge\" for = \"treatment-date\" style = \"margin-left: 8px; background-color: rgba(255, 255, 255, 0.8); margin-top: 8px; color: #db3e04;\">Remedy ID: <span style = \"color:black\">` + geneterateNewRID() + `</span></p>
+						<p class = \"badge\" for = \"treatment-date\" style = \"margin-left: 8px; background-color: rgba(255, 255, 255, 0.8); margin-top: 8px; color: #db3e04;\">Remedy ID: <span style = \"color:black\">` + generateNewRID() + `</span></p>
 						<table>
 						<tr><td><label class = \"badge\" for = \"treatment\" style = \"margin-right:5px; background-color: white; margin-top: 8px; color: #db3e04;\">Treatment:</label></tr></td>
 						<tr><td><textarea rows = "4" cols = "30" type = \"text\" name = \"treatment\" id = \"treatment\" style = \"font-size: 25px; font-family: \'Inter\'; border-radius: 10px; margin-top: 5px\"></textarea></tr></td>
@@ -71,18 +63,52 @@ function issueTreatment(){
 	alert(getName(PID) + ' is prescribed ' + treatmentPrescribed + " on " + treatmentDate + ' with status ' + treatmentStatus);
 }
 
-function notFound(PID){
+function getPatientInfo(PID){
 	// do database query here
+	return new Promise((resolve, reject) => {
+		if(PID == 0){
+			return "-1";
+		}
+		url = "http://127.0.0.1:9000/patients/" + PID + "/";
+		let patientName = "-2";
+		const xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.onload = () => {
+			if (xhr.status === 200) {
+				const data = JSON.parse(xhr.responseText);
+				resolve(data);
+			} else {
+				resolve("-1");
+			}
+		};
+		xhr.onerror = () => {
+			reject(new Error('Request failed'));
+		};
+		xhr.send();
+	});
 	return false;
 }
 
-function FetchPatient(){
+async function FetchPatient(){
 	PID = document.forms['patient-form'].PID.value;
-	const patientInfo = document.getElementById('patient-info');
-	if(notFound(PID) == true){
-		patientInfo.innerHTML = "Patient Not Found!";
+	const patientInfoDiv = document.getElementById('patient-info');
+	patientInfoReceived = await getPatientInfo(PID);
+	if(patientInfoReceived === "-1"){
+		patientInfoDiv.innerHTML = "Patient Not Found!";
+		patientInfoDiv.scrollIntoView();
 		return;
 	}
-	patientInfo.innerHTML = getPatientInfo(PID) + treatmentForm();
-	window.scrollTo(0, document.body.scrollHeight);
+
+	patientInfoDiv.innerHTML = 	`
+							<ul>
+							<li style = \"font-size: 25px; font-family: \'Inter\'; padding-left: 20px;\"> Patient ID: ` + patientInfoReceived.PID + ` 
+							<li style = \"font-size: 25px; font-family: \'Inter\'; padding-left: 20px;\"> Patient Name: ` + patientInfoReceived.Name + ` 
+							<li style = \"font-size: 25px; font-family: \'Inter\'; padding-left: 20px;\"> Blood Group: ` + patientInfoReceived.Blood_Group + ` 
+							<li style = \"font-size: 25px; font-family: \'Inter\'; padding-left: 20px;\"> Govt. ID (number): ` + patientInfoReceived.Gov_ID + ` 
+							<li style = \"font-size: 25px; font-family: \'Inter\'; padding-left: 20px;\"> Govt. ID (type): ` + patientInfoReceived.Gov_ID_Type + ` 
+							<li style = \"font-size: 25px; font-family: \'Inter\'; padding-left: 20px;\"> Current Health: ` + patientInfoReceived.Current_Health + ` 
+							</ul>
+						`
+	patientInfoDiv.innerHTML += treatmentForm();
+	patientInfoDiv.scrollIntoView();
 }

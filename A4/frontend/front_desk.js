5672248af0c5b3b82f20e5728ab97b5cf60e642d
frediff <subhamghosh.dgr@gmail.com>
2023-03-05 19:36:17 +0530

    fixed fetching data

--- A4/frontend/front_desk.js
+++ A4/frontend/front_desk.js
@@ -4,47 +4,31 @@ function getName(EID){
 	// do database query here
 	return "Name(" + EID + ")";
 }
-function getPatientName(PID){
-	//do database query
-	if(PID == 0){
-		return "-1";
-	}
-	url = "http://127.0.0.1:9000/patients/" + PID + "/";
-	let patientName = "-2";
-	fetch(url, {
-		method: "GET",
-		// headers: {
-		// 	"Content-type": "application/json", 
-		// 	// "Access-Control-Allow-Origin": "*",
-		// },
-		mode: "no-cors"
-	})	
-	.then((response) => {
-		response.text();
-		console.log(response);
-	}).catch((error) => {
-		console.log(error);
-	});
-	// .then((myJson) => {
-	// 	myImage.src = URL.createObjectURL(myJson);
-	// });
 
-	// .then(response => {
-	// 	response.json();
-	// 	alert(response.json());
-	// });
-	// .then((data) => {
-	// 	alert(data.status);
-	// 	// if(data.status != 200){ // add patient not found logic
-	// 	// 	return "-1";
-	// 	// }else{
-	// 	// 	return data.Name; // add patient name
-	// 	// }
-	// });
-
-	return patientName;
-
+function getPatientName(PID) {
+  return new Promise((resolve, reject) => {
+		if(PID == 0){
+			return "-1";
+		}
+		url = "http://127.0.0.1:9000/patients/" + PID + "/";
+		let patientName = "-2";
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        resolve(data.Name);
+      } else {
+        reject(new Error(`Request failed with status ${xhr.status}`));
+      }
+    };
+    xhr.onerror = () => {
+      reject(new Error('Request failed'));
+    };
+    xhr.send();
+  });
 }
+
 
 window.onload = function(){
 	var url = document.location.href,
@@ -86,9 +70,9 @@ function RegisterPatient(){
 	alert(`PID = ` + PID + `\nName = ` + name + `\ngov_id = ` + gov_id + `\ngov_id_type = ` + gov_id_type + `\nblood_group = ` + blood_group + `\ncurr_health = ` + curr_health + `\n`);
 }
 
-function AdmitPatient(){
+async function AdmitPatient(){
 	const PID = document.forms['admit-form'].PID.value;
-	const patientName = getPatientName(PID);
+	const patientName = await getPatientName(PID);
 
 	if(patientName === "-1"){
 		alert('Patient Not Found!');

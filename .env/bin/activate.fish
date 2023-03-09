62a283fd4bbad47f7bf1470ea406eb0f3308e2dc
ammetre <aritramitra2002@kgpian.iitkgp.ac.in>
2023-03-09 12:47:31 +0530

    Some vscode thingy;

--- .env/bin/activate.fish
+++ .env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/aritramitra/Documents/Sem 6/DBL/CS39202-Database-Management-Systems-Laboratory/.env"
+set -gx VIRTUAL_ENV "/home/aritramitra/CS39202-Database-Management-Systems-Laboratory/.env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

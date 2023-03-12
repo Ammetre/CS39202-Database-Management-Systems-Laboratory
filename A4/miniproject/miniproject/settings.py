14156c97af74dfefadb128a30fb6feac82717f6b
frediff <subhamghosh.dgr@gmail.com>
2023-03-12 11:25:11 +0530

    er push

--- A4/miniproject/miniproject/settings.py
+++ A4/miniproject/miniproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'django_extensions',
     'hospital',
     'crispy_forms',
     'crispy_bootstrap5',
